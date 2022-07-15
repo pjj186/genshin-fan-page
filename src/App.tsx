011449662b47369b97f7bbbe7247bfe0c67ec139
Jong jae Park <pjj186@naver.com>
2022-07-16 01:24:09 +0900

    Header & Video

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Reset } from "styled-reset";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="w-screen h-screen">
       <Reset />
-      <div className=" text-orange-600">알게머ㅜ야</div>
-    </>
+      <Header />
+      <div>
+        <video autoPlay muted className="w-screen absolute -z-10">
+          <source src="/video/genshinvideo.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
   );
 }
 
